da6b8b62c0e43fd497a6a36388a9165027f2edcc
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-10-11 12:29:54 -0300

    Initial commit

---
+++ src/main/java/br/com/josenaldo/catbasket/CatBasketApplication.java
@@ -0,0 +1,13 @@
+package br.com.josenaldo.catbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatBasketApplication.class, args);
+	}
+
+}
